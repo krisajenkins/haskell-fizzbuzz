43f0d1fe2e9d51ac7b322ca03dcd1cc70600ad59
Kris Jenkins <krisajenkins@gmail.com>
2014-10-29 22:14:07 +0000

    Tidying up the code using an Either.
    Thanks Phil!

--- Fizzbuzz.hs
+++ Fizzbuzz.hs
@@ -1,24 +1,18 @@
-import Data.Maybe
+data FB = Fizz | Buzz | Fizzbuzz deriving (Show,Eq)
 
-data Intview = Fizz | Buzz | Fizzbuzz | Intview Int deriving (Eq)
-
-instance Show Intview where
-  show (Intview x) = show x
-  show Fizz = "Fizz"
-  show Buzz = "Buzz"
-  show Fizzbuzz = "Fizzbuzz"
-
-matcher :: Int -> Int -> Maybe Intview
+matcher :: Int -> Int -> Maybe FB
 matcher 0 0 = Just Fizzbuzz
 matcher 0 _ = Just Fizz
 matcher _ 0 = Just Buzz
 matcher _ _ = Nothing
 
-translate :: Int -> Intview
-translate n = fromMaybe (Intview n) (matcher (mod n 3) (mod n 5))
+veilOver :: Int -> Either FB Int
+veilOver n = case matcher (mod n 3) (mod n 5) of
+                Nothing -> Right n
+                Just x  -> Left x
 
-fizzbuzz :: [Int] -> [Intview]
-fizzbuzz = fmap translate
+fizzbuzz :: [Int] -> [Either FB Int]
+fizzbuzz = fmap veilOver
 
 main :: IO ()
-main = print $ fizzbuzz [1..20]
+main = print (fmap (either show show) (fizzbuzz [1..20]))
