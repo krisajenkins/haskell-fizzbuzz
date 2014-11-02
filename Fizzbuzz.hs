fc0bb0fcca0f344ac9c7660182e4b87a444e1743
Kris Jenkins <krisajenkins@gmail.com>
2014-11-02 08:34:23 +0000

    A few enhancements.

--- Fizzbuzz.hs
+++ Fizzbuzz.hs
@@ -6,26 +6,25 @@ joinMaybe (Just x) Nothing = Just [x]
 joinMaybe Nothing (Just y) = Just [y]
 joinMaybe Nothing Nothing = Nothing
 
-maybeToEither :: Maybe a -> b -> Either a b
-maybeToEither (Just x) _ = Left x
-maybeToEither Nothing y = Right y
+maybeToEither :: a -> Maybe b -> Either a b
+maybeToEither x = maybe (Left x) Right
 
 data FB = Fizz | Buzz | Fizzbuzz deriving (Show,Eq)
 
 fizzes :: [Maybe FB]
-fizzes = cycle [Nothing, Nothing, Just Fizz]
+fizzes = cycle [Just Fizz, Nothing, Nothing]
 
 buzzes :: [Maybe FB]
-buzzes = cycle [Nothing, Nothing, Nothing, Nothing, Just Buzz]
+buzzes = cycle [Just Buzz, Nothing, Nothing, Nothing, Nothing]
 
 fizzbuzzes :: [Maybe [FB]]
 fizzbuzzes = zipWith joinMaybe fizzes buzzes
 
 ints :: [Int]
-ints = [1..]
+ints = [0..]
 
-fizzbuzz :: [Either [FB] Int]
-fizzbuzz = zipWith maybeToEither fizzbuzzes ints
+fizzbuzz :: [Either Int [FB]]
+fizzbuzz = zipWith maybeToEither ints fizzbuzzes
 
 main :: IO ()
 main = print $ either show show <$> take 30 fizzbuzz
